1841dc0a66b5a1d3424c7d5b4c9188432f544477
Andrey Guzev <kodermanishe@gmail.com>
2019-06-18 21:59:02 -0500

    Job Application 1.1

--- job_application/src/main/java/edu/mum/JobApplicationApp.java
+++ job_application/src/main/java/edu/mum/JobApplicationApp.java
@@ -9,16 +9,7 @@ import java.util.Arrays;
 @SpringBootApplication
 public class JobApplicationApp {
 
-
     public static void main(String[] args) {
         SpringApplication.run(JobApplicationApp.class, args);
-
-        /*System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-        String[] beanNames = ctx.getBeanDefinitionNames();
-        Arrays.sort(beanNames);
-        for (String beanName : beanNames) {
-            System.out.println(beanName);
-        }*/
     }
 }
